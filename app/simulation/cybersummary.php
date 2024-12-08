aea03957e2a8784258c168c55913bb8d3bf97d9d
codystack <ebuzzadvert@gmail.com>
2024-12-08 12:31:47 +0100

    playbook added

--- app/simulation/cybersummary.php
+++ app/simulation/cybersummary.php
@@ -1,3 +1,20 @@
+<?php
+//Connect Database
+include ('.././config/db.php');
+
+session_start();
+
+if (!isset($_SESSION['username'])) {
+    header('location: logout');
+}
+
+if (isset($_GET['logout'])) {
+    session_destroy();
+    unset($_SESSION['username']);
+    header("location: login");
+}
+
+?>
 <!DOCTYPE html>
 <html>
 
